49e119040d99130fb4c096bb42d140e93a0ae8f4
userb <daniel.borjxl24@gmail.com>
2024-09-12 09:27:25 -0600

    h

--- computo2/semana8.py
+++ computo2/semana8.py
@@ -1,11 +1,29 @@
 import sys
-from PyQt5.QtWidgets import (QApplication, QMainwindow, QWidget, QPushbutton)
+from PyQt5.Qtwidgets import (QApplication, QMainWindow, QWidget, QPushbutton, QLineEdit, QHBoxLayout)
 
 
-class ventanam(QMainwindow):
+class ventanam(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Practica semana 8")
+        boton = QPushbutton("haz click aqui")
+        boton.clicked.connect(self.evento_click)
+        inputxt = QLineEdit()
+        inputxt.textChanged.connect(self.evento_click)
+        central = QWidget()
+        layout= QHBoxLayout()
+        layout.addWidget(boton)
+        layout.addWidget(inputxt)
+        central.setLayout(layout)
+        self.setCentralWidget(central)
+        #self.setCentralWidget(boton)
+
+
+    def evento_click(self):
+        print("haz hecho click")
+
+    def evento_txt(self):
+        pass
 
 
 app = QApplication(sys.argv)
